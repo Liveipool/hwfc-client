8fec5543c2c059250fb687f5f84458839a23090f
hhk <codeforsomeone@gmail.com>
2017-04-24 14:20:24 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>主页</h1>
+    <router-link to="/teamIntroduction">团队介绍</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/TeamIntroduction.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>团队介绍</h1>
+    <router-link to="/home">主页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TeamIntroduction',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
