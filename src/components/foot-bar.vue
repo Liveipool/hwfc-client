7a96c6f4478da6c8b92229c311148a5121d687c4
ougwen1235 <ougwen1235@gmail.com>
2017-05-01 23:42:00 +0800

    完成底部栏

--- src/components/foot-bar.vue
+++ src/components/foot-bar.vue
@@ -1,6 +1,11 @@
 <template>
   <div id='footBar' class="navbar-fixed-bottom visible-lg-block">
-    <h1>底栏</h1>
+    <div class="foot-bar-name chinese-name">我在中大，混碗饭吃</div>
+    <div class="foot-bar-name english-name">Hello World Football Club</div>
+    <div class="foot-bar-bottom">
+      <span>@ Copyright Hello World Football Club</span>
+      <router-link to="">没有账号?</router-link>
+    </div>
   </div>
 </template>
 
@@ -16,9 +21,43 @@ export default {
 
 <style scoped>
 #footBar {
-  width: calc(100% - 2px);
-  height: 155px;
-  border: 1px solid #39f;
+  width: 100%;
+  font-family: Arial;
+}
+
+.foot-bar-name {
+  color: #39f;
   text-align: center;
+}
+
+.chinese-name {
+  font-weight: bold;
+  font-size: 50px;
+}
+
+.english-name {
+  font-size: 30px;
+  margin-bottom: 12px;
+}
+
+.foot-bar-bottom {
+  height: 38px;
+  background-color: #39f;
+  font-size: 20px;
+  color: #313231;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+span {
+  margin-left: 25px;
+}
+
+/*router-link本身就是一个a标签*/
+a {
+  margin-right: 33px;
+  color: #313231;
+  text-decoration: underline;
 }
 </style>
